4329577d5c0f5a19d3cadf96a8442a0800fb6bec
Alex <alex.permyakoff@gmail.com>
2025-12-24 14:47:44 -0800

    add health cheks

--- main.go
+++ main.go
@@ -12,8 +12,20 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Server Time: %s\n", currentTime)
 }
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK")
+}
+
+func readyHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Ready")
+}
+
 func main() {
 	http.HandleFunc("/", timeHandler)
+	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/ready", readyHandler)
 	fmt.Println("Server starting on port 1599...")
 	if err := http.ListenAndServe(":1599", nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
